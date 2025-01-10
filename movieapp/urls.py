54b50489c0070052e049f4f21108270aa1d632c1
matthcol <matthias.colin@gmail.com>
2025-01-10 11:07:39 +0100

    add routes

--- movieapp/urls.py
+++ movieapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:movie_id>/", views.movie_detail, name="movie-detail"),
+    path("add/", views.movie_add, name="movie-add")
 ]
--- movieapp/views.py
+++ movieapp/views.py
@@ -7,3 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Movie web site very soon here !")
+
+def movie_detail(request, movie_id):
+    return HttpResponse("Movie detail with id:", movie_id)
+
+def movie_add(request):
+    return HttpResponse("Movie add page")
